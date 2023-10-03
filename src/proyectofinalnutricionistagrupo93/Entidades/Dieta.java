fcc4c95db7b2fc53a2c30076e71d26de7c6cc3e8
GabiiR <grgithub@gmail.com>
2023-10-02 23:44:41 -0300

    Se añadieron "Getter's" y "Setter's" a "Dieta".

--- src/proyectofinalnutricionistagrupo93/Entidades/Dieta.java
+++ src/proyectofinalnutricionistagrupo93/Entidades/Dieta.java
@@ -35,5 +35,60 @@ public class Dieta {
         this.pesoInicial = pesoInicial;
         this.pesoFinal = pesoFinal;
     }
-    
+
+    public int getIdDieta() {
+        return idDieta;
+    }
+
+    public void setIdDieta(int idDieta) {
+        this.idDieta = idDieta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Paciente getPaciente() {
+        return Paciente;
+    }
+
+    public void setPaciente(Paciente Paciente) {
+        this.Paciente = Paciente;
+    }
+
+    public LocalDate getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(LocalDate fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public LocalDate getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(LocalDate fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public double getPesoInicial() {
+        return pesoInicial;
+    }
+
+    public void setPesoInicial(double pesoInicial) {
+        this.pesoInicial = pesoInicial;
+    }
+
+    public double getPesoFinal() {
+        return pesoFinal;
+    }
+
+    public void setPesoFinal(double pesoFinal) {
+        this.pesoFinal = pesoFinal;
+    }
 }
