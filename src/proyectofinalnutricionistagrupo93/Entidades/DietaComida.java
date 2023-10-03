7acd142bbc8fd910d8911e6c83ff2e0f0699ac16
GabiiR <grgithub@gmail.com>
2023-10-03 05:35:43 -0300

    Se agregaron "Getter's" y "Setter's".

--- src/proyectofinalnutricionistagrupo93/Entidades/DietaComida.java
+++ src/proyectofinalnutricionistagrupo93/Entidades/DietaComida.java
@@ -1,8 +1,46 @@
 package proyectofinalnutricionistagrupo93.Entidades;
 
 public class DietaComida {
-    protected int id;
+    protected int id; //ID.
     
     protected Comida comida;
     protected Dieta dieta;
+
+    public DietaComida() {
+    }
+
+    public DietaComida(Comida comida, Dieta dieta) {
+        this.comida = comida;
+        this.dieta = dieta;
+    }
+
+    public DietaComida(int id, Comida comida, Dieta dieta) {
+        this.id = id;
+        this.comida = comida;
+        this.dieta = dieta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Comida getComida() {
+        return comida;
+    }
+
+    public void setComida(Comida comida) {
+        this.comida = comida;
+    }
+
+    public Dieta getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(Dieta dieta) {
+        this.dieta = dieta;
+    }
 }
