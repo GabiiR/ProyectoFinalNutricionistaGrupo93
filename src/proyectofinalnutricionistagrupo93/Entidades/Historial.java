c9746ae5b91880945d5588e0cab6108a95ff444c
GabiiR <grgithub@gmail.com>
2023-10-10 20:59:47 -0300

    Se modifico la clase "Historial".

--- src/proyectofinalnutricionistagrupo93/Entidades/Historial.java
+++ src/proyectofinalnutricionistagrupo93/Entidades/Historial.java
@@ -14,22 +14,22 @@ import java.time.LocalDate;
 public class Historial {
     protected int idHistorial;
     protected Paciente paciente;
-    protected double peso;
+    protected double pesoControl;
     protected LocalDate fechaRegistro;
 
     public Historial() {
     }
 
-    public Historial(Paciente paciente, double peso, LocalDate fechaRegistro) {
+    public Historial(int idHistorial, Paciente paciente, double pesoControl, LocalDate fechaRegistro) {
+        this.idHistorial = idHistorial;
         this.paciente = paciente;
-        this.peso = peso;
+        this.pesoControl = pesoControl;
         this.fechaRegistro = fechaRegistro;
     }
 
-    public Historial(int idHistorial, Paciente paciente, double peso, LocalDate fechaRegistro) {
-        this.idHistorial = idHistorial;
+    public Historial(Paciente paciente, double pesoControl, LocalDate fechaRegistro) {
         this.paciente = paciente;
-        this.peso = peso;
+        this.pesoControl = pesoControl;
         this.fechaRegistro = fechaRegistro;
     }
 
@@ -49,12 +49,12 @@ public class Historial {
         this.paciente = paciente;
     }
 
-    public double getPeso() {
-        return peso;
+    public double getPesoControl() {
+        return pesoControl;
     }
 
-    public void setPeso(double peso) {
-        this.peso = peso;
+    public void setPesoControl(double pesoControl) {
+        this.pesoControl = pesoControl;
     }
 
     public LocalDate getFechaRegistro() {
@@ -64,10 +64,9 @@ public class Historial {
     public void setFechaRegistro(LocalDate fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
-
+    
     @Override
     public String toString() {
-        return "Historial{" + "paciente=" + paciente + ", peso=" + peso + ", fechaRegistro=" + fechaRegistro + '}';
+        return "pesoControl= " + pesoControl + " fecha registro= " + fechaRegistro;
     }
-   
 }
